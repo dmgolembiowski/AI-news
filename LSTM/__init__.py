18293ac07a0066114f2dd5fdb584e0e8d8fff3e4
David M. Golembiowski <dmgolembiowski@gmail.com>
2019-03-02 00:05:02 -0500

    Update __init__.py

--- LSTM/__init__.py
+++ LSTM/__init__.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 import numpy as np
 from LSTM import InputGate, OutputGate, ForgetGate, ConstantErrorCarousel, MemoryCell
+
+class SydneyClass:
+  def __init__(self, wordParams, sentenceParams):
+    self.wordParams = wordParams
+    self.sentenceParams = sentenceParams
