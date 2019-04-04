5819e9cc598177be000ee6326dfccea9bdf65ae3
David Golembiowski <dmgolembiowski@gmail.com>
2019-04-04 03:46:28 -0400

    Have fun, future self.
     On branch master
     Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   data/dbExtract.py
    	new file:   data/usedIDs.txt

--- data/dbExtract.py
+++ data/dbExtract.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import sqlite3
 import sys
+import random
 
 def getDatabase(conn, content=True, title=False, tableName="longform"):
     if content:
@@ -46,4 +47,37 @@ def extract(path=None):
     local_connection = getConnection() if (path == None) else getConnection(PATH=path)
     news, fieldName = getDatabase(conn=local_connection)
     return (entry for entry in news[fieldName])
+
+def idCheck(idInt):
+    PATH = "used.txt"
+    with open(PATH, "r+") as f:
+        try:
+            used = f.read()
+            try:
+                assert(idInt not in used)
+                return idInt
+            except AssertionError:
+                f.write(idInt)     
+                return -1
+        except Exception:
+            pass
+
+def writeUsed(path, *used):
+    PATH = "used.txt"
+    with open(path, "r+") as f:
+        for u in used:
+            f.write(u)
+
+def partition(path=None, default_size=179599):
+    path = path if (path != None) else "news.db"
+    availabe_IDs = [id if not idCheck(id)==-1 for id in range(0,default_size)]
+    random.shuffle(availabe_IDs)
+    with open(path, "r+") as f:
+        try:
+            used = f.read()
+        except Exception:
+            used = []
+    rand = random.sample(availabe_IDs, round(0.1*default_size / 3, ndigits=0))
+    # pass on rand to be the ID's that get matched with a SQL query
+    # write these to their own unique temporary db.file
 
