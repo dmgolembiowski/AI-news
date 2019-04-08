df5226f74f38594aa997f8b18cc141928539870d
David Golembiowski <dmgolembiowski@gmail.com>
2019-04-08 17:04:40 -0400

    Changes to be committed: 	new file:   GRU/.gate.py.swp 	new file:   GRU/training.py 	modified:   data/dbExtract.py

---
+++ GRU/training.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import torch
+
+def indexesFromSentence(lang, sentence):
+    pass
--- data/dbExtract.py
+++ data/dbExtract.py
@@ -3,22 +3,23 @@ import pandas as pd
 import sqlite3
 import sys
 import random
+import collections
 
-def getDatabase(conn, content=True, title=False, tableName="longform"):
+def getDatabase(conn, content=True, title=False, tableName="clean"):
     if content:
         fieldName = "content"
     else:
         fieldName = "title"
-    news = pd.read_sql(f"SELECT {fieldName} FROM {tableName};", conn)
+    news = pd.read_sql(f"SELECT {fieldName} FROM {tableName} WHERE LENGTH({fieldName}) >= 6000;", conn)
     return news, fieldName
     
-def getConnection(local=True, PATH="all-the-news.db"):
+def getConnection(local=True, PATH="news.db"):
     if local:
         """
         Creates a database connection to the 099.db database file
         located at ~/./all-the-news.db
         """
-        PATH="all-the-news.db"
+        PATH="news.db"
         try:
             connection = sqlite3.connect(PATH)
             assert(connection is not None)
@@ -67,7 +68,7 @@ def writeUsed(path, *used):
     with open(path, "r+") as f:
         for u in used:
             f.write(u)
-
+"""
 def partition(path=None, default_size=179599):
     path = path if (path != None) else "news.db"
     availabe_IDs = [id if not idCheck(id)==-1 for id in range(0,default_size)]
@@ -80,4 +81,53 @@ def partition(path=None, default_size=179599):
     rand = random.sample(availabe_IDs, round(0.1*default_size / 3, ndigits=0))
     # pass on rand to be the ID's that get matched with a SQL query
     # write these to their own unique temporary db.file
+"""
+
+
+
+
+def newArticle(allNews):
+    def wrapper():
+        yield collections.deque(word for word in next(allNews).split(' ') if word !='')
+    def getNext(generator_obj):
+        try:
+            return next(generator_obj)
+        except StopIteration:
+            return getNext(wrapper())
+        except TypeError:
+            return getNext(wrapper())
+        except Exception:
+            raise
+    return getNext(wrapper())
+
+def nextWords(article, allNews, nextChunk=None, wordcount = 50, wc=-1, dropFirst=False):
+    """Example usage:
+    allNews = dbExtract.extract()
+    new_article = dbExtract.newArticle(allNews)
+
+    Steps:
+    1. article.popleft() first {wordcount} words from article
+    2. 
+    """
+    def fillup(nextChunk=None, wc=wc):
+        nextChunk = collections.deque(maxlen=wordcount) if nextChunk == None else nextChunk
+        wc = 0 if len(nextChunk) == 0 else len(nextChunk)-1
+        while wc < wordcount:
+            try:
+                nextChunk.append(article.popleft())
+                wc += 1
+                if wc == wordcount:
+                    return nextChunk
+                else:
+                    return fillup(nextChunk=nextChunk, wc=wc)
+            except IndexError:
+                wc += 1
+                return nextWords(newArticle(allNews), 
+                        allNews=allNews, 
+                        nextChunk=nextChunk, 
+                        wordcount=wordcount,
+                        wc=wc)
+    if dropFirst:
+        nextChunk.popleft()
+    return fillup(nextChunk, wc)
 
