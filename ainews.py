b4f4fd0bbe6f38b3a38f18ca948ba5e4816d1203
root <root@debian.dGolembiowski.com>
2019-04-16 19:56:19 -0400

    Creating a main file that regulates the training

--- ainews.py
+++ ainews.py
@@ -5,7 +5,7 @@ import torch.nn as nn # Neural Net
 from data import dbExtract
 from GRU import Update, Reset
 #from learning import 
-import groupWork
+#import groupWork
 from collections import deque
 import numpy as np
 import subprocess
@@ -15,16 +15,34 @@ from torch.multiprocessing import Process, Pool, set_start_method
 import sys
 import random
 
+
+
 def main(_resumeTraining=False, 
             training_article_range=None, 
             modelFile='', 
             trainingData="./trainingData/"):
     """
-`trained_article_range` expects either none or 2 element list of
+    `trained_article_range` expects either none or 2 element list of
     inclusive range for all seen training article .txt files
     (Semipro-tip usage:)
->>> ainews.main(_resumeTraining=True, training_article_range=[400,500],
-        modelFile='rnn_20_epoch.net')
+
+            >>> ainews.main(_resumeTraining=True, training_article_range=[400,500],
+            modelFile='rnn_20_epoch.net')
+
+    Training Routine Steps:
+    1. Instantiate the Dataset
+
+    2. Instantiate the model
+
+    3. Instantiate the loss function
+
+    4. Instantiate the optimizer
+
+    5. Iterate over the dataset's training partition and update the model parameters
+
+    6. Iterate over the dataset's validation partition and measure the performance
+
+    7. For new articles, repeat steps 50-100 more times.
     """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     try:
