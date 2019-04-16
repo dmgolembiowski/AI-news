309f605e4f244446ed828daae7e89531494ffe54
root <root@debian.dGolembiowski.com>
2019-04-16 01:59:00 -0400

    It's 2am. Brain has lost all ability to (what was I saying again?)

--- ainews.py
+++ ainews.py
@@ -8,28 +8,36 @@ from GRU import Update, Reset
 import groupWork
 from collections import deque
 import numpy as np
+import subprocess
+# Late night shenanigans
+import multiprocessing as mp
+from torch.multiprocessing import Process, Pool, set_start_method
+import sys
+import random
 
-articles = collections.deque()
-wordToInt = {}
-
-
-
-def main(trainFirst=False, ):
+def main(_resumeTraining=False, 
+            training_article_range=None, 
+            modelFile='', 
+            trainingData="./trainingData/"):
+    """
+`trained_article_range` expects either none or 2 element list of
+    inclusive range for all seen training article .txt files
+    (Semipro-tip usage:)
+>>> ainews.main(_resumeTraining=True, training_article_range=[400,500],
+        modelFile='rnn_20_epoch.net')
+    """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    if trainFirst:
-        newsGenerator = data.main()
-    allNews = dbExtract.extract()
-    current_article = []
-    i = 0
-    max_count = 10
-    while i < max_count:
-        articles.append(dbExtract.newArticle(allNews))
-        if i == max_count:
-            break
-        else:
-            i += 1
-    current_article = [x for x in article[-1]]
-    size = len(current_article)
-    i = 0
-    while i < size:
-        representations[]
+    try:
+        if _resumeTraining:
+            # Create dictionaries to gather training IDs and validation IDs
+            lower_bd = training_article_range[0]
+            upper_bd = training_article_range[1]
+            partition = {'train':[f"id-{n}" for n in range()], 'validation':[]}
+            labels = {}
+
+
+        set_start_method('spawn', force=True)
+    except RuntimeError:
+        pass
+
+
